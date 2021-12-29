e195bd033c75566df84f1b6da61538abe3c505b2
Faisal L <salluthdev@gmail.com>
2021-12-29 22:03:52 +0700

    Penyesuian Style Swiper2 - Video

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,31 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
+const swiper1 = new Swiper('.mySwiper1', {
   direction: 'horizontal',
   loop: true,
 
-  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
+
+var swiper2 = new Swiper(".mySwiper2", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 400,
+    modifier: 1,
+    slideShadows: false,
+  },
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
   pagination: {
     el: '.swiper-pagination',
   },
